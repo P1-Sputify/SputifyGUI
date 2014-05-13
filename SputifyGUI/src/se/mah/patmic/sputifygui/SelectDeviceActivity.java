aec63d99e902a45549047ee3e1e8657f7b029d0b
Patrik <patriklarsson88@gmail.com>
2014-05-13 11:49:38 +0200

    Lade till mina ändringar i SelectDeviceActivity och la till Klassen Bluetooth service

--- SputifyGUI/src/se/mah/patmic/sputifygui/SelectDeviceActivity.java
+++ SputifyGUI/src/se/mah/patmic/sputifygui/SelectDeviceActivity.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
 /**
- * Klassen anv√§nds f√∂r att v√§lja ett item i en lista. I detta fall ska man v√§lja
+ * Klassen anv‰nds fˆr att v‰lja ett item i en lista. I detta fall ska man v‰lja
  * en bluetooth enhet och skicka tillbaka adressen.
  * 
  * @author Patrik
@@ -26,16 +26,17 @@ import android.widget.AdapterView.OnItemClickListener;
 public class SelectDeviceActivity extends Activity {
 	private static String TAG = "SelectDeviceActivity";
 
-	// F√∂r att h√§mta adressen till den valda enheten
-//	public static String EXTRA_DEVICE_ADRESS = "se.mah.ad1107.device_adress";
+	// Fˆr att h‰mta adressen till den valda enheten
+	public static String EXTRA_DEVICE_ADRESS = "se.mah.ad1107.device_adress";
 
-	// Anv√§nds f√∂r att listan
+	// Anv‰nds fˆr att listan
 	private BluetoothAdapter mBtAdapter;
 	private ArrayAdapter<String> mPairedDevicesArrayAdapter;
 	private ListView listView;
+	private BluetoothService
 
 	/**
-	 * Anropas n√§r aktiviten skapas. Skapar listan och l√§gger till items i
+	 * Anropas n‰r aktiviten skapas. Skapar listan och l‰gger till items i
 	 * listan.
 	 */
 	@Override
@@ -58,39 +59,43 @@ public class SelectDeviceActivity extends Activity {
 						+ device.getAddress());
 			}
 		} else {
-			// L√§gg till Meddelende ifall det inte finns n√•gra parade enheter
+			// L‰gg till Meddelende ifall det inte finns nÂgra parade enheter
 		}
 		listView.setOnItemClickListener(mDeviceClickListener);
 	}
 
 	/**
-	 * En lysnnare f√∂r vad som ska h√§nda n√§r man klickar p√• ett item i listan.
-	 * Skickar tillbaka en str√§ng som inneh√•ller en adress till en bluetooth
+	 * En lysnnare fˆr vad som ska h‰nda n‰r man klickar pÂ ett item i listan.
+	 * Skickar tillbaka en str‰ng som innehÂller en adress till en bluetooth
 	 * device till den aktiviten som startade denna.
 	 */
 	private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {
 		public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
 
-			// H√§mta Mac adressen f√∂r enhenten man har valt. Den √§r de sista 17
+			// H‰mta Mac adressen fˆr enhenten man har valt. Den ‰r de sista 17
 			// tecknena i TextViewn
 			String info = ((TextView) v).getText().toString();
-			String address = info.substring(info.length() - 17); // Adressen √§r
+			String address = info.substring(info.length() - 17); // Adressen ‰r
 																	// de sista
 																	// 17
 																	// teckena i
-																	// str√§ngen
+																	// str‰ngen
 			Log.d(TAG, info + "\n" + address);
 
-			// Skapar en intent s√• att man kan skicka tillbaka data
-			Intent intent = new Intent();
-//			intent.putExtra(EXTRA_DEVICE_ADRESS, address);
-//			Log.d(TAG,
-//					"Data in intent"
-//							+ intent.getExtras().getString(EXTRA_DEVICE_ADRESS));
+			// Skapar en intent sÂ att man kan skicka tillbaka data
+			Intent intent = new Intent(SelectDeviceActivity.this, PlaylistActivity.class);
+			intent.putExtra(EXTRA_DEVICE_ADRESS, address);
+			Log.d(TAG,
+					"Data in intent"
+							+ intent.getExtras().getString(EXTRA_DEVICE_ADRESS));
+			
 
-			// S√§tt resultatet till ok och skicka tillbaka datan
-			setResult(RESULT_OK, intent);
-			finish();
+//			// S‰tt resultatet till ok och skicka tillbaka datan
+//			setResult(RESULT_OK, intent);
+//			finish();
+			
+			//   Starta en ny activity skicka med adressen och ett bluetooth service
+			startActivity(intent);
 		}
 	};
 
