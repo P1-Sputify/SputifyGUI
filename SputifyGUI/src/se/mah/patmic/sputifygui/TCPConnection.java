033fe65a9568b0ee3756dbfb9db66c8688a5bd87
Michel Falk <michel.falk@gmail.com>
2014-05-26 10:58:51 +0200

    Förbättrade logiken i TCP tråden

--- SputifyGUI/src/se/mah/patmic/sputifygui/TCPConnection.java
+++ SputifyGUI/src/se/mah/patmic/sputifygui/TCPConnection.java
@@ -83,7 +83,7 @@ public enum TCPConnection {
 
 	public void requestTrack(int trackNr) {
 		trackRequest = trackNr;
-		requestedTrackStatus = TRACK_NOT_RECIEVED;
+		requestedTrackStatus = TRACK_DOWNLOADING;
 	}
 
 	public byte[] getRequestedTrack() {
@@ -114,29 +114,31 @@ public enum TCPConnection {
 		public void run() {
 			while (true) {
 				if (socket != null && socket.isConnected()) {
-					if (loginStatus != LOGGED_IN && user != null && pass != null) {
-						loginStatus = LOGGING_IN;
-						String[] loginArray = new String[2];
-						loginArray[0] = user;
-						loginArray[1] = pass;
+					if (loginStatus != LOGGED_IN) {
+						if (user != null && pass != null) {
+							loginStatus = LOGGING_IN;
+							String[] loginArray = new String[2];
+							loginArray[0] = user;
+							loginArray[1] = pass;
 
-						try {
-							oos.writeObject(loginArray);
-							String reply = (String) ois.readObject();
-							if (reply.equalsIgnoreCase("login success")) {
-								loginStatus = LOGGED_IN;
-							} else if (reply.equalsIgnoreCase("login failed")) {
+							try {
+								oos.writeObject(loginArray);
+								String reply = (String) ois.readObject();
+								if (reply.equalsIgnoreCase("login success")) {
+									loginStatus = LOGGED_IN;
+								} else if (reply.equalsIgnoreCase("login failed")) {
+									loginStatus = NOT_LOGGED_IN;
+									user = pass = null;
+								} else {
+									loginStatus = NOT_LOGGED_IN;
+									user = pass = null;
+									System.out.println("Server sent unkown reply: " + reply);
+								}
+							} catch (IOException | ClassNotFoundException e) {
+								e.printStackTrace();
 								loginStatus = NOT_LOGGED_IN;
 								user = pass = null;
-							} else {
-								loginStatus = NOT_LOGGED_IN;
-								user = pass = null;
-								System.out.println("Server sent unkown reply: " + reply);
 							}
-						} catch (IOException | ClassNotFoundException e) {
-							e.printStackTrace();
-							loginStatus = NOT_LOGGED_IN;
-							user = pass = null;
 						}
 					} else { // loginStatus == LOGGED_IN && socket connected
 						if (playListStatus != LIST_RECIEVED) {
@@ -154,7 +156,7 @@ public enum TCPConnection {
 								e.printStackTrace();
 								playListStatus = LIST_NOT_RECIEVED;
 							}
-						} else if (requestedTrackStatus == TRACK_NOT_RECIEVED) {
+						} else if (requestedTrackStatus != TRACK_RECIEVED) {
 							if (track != null && currentTrack == trackRequest) {
 								requestedTrackStatus = TRACK_RECIEVED;
 							} else {
