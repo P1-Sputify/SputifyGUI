898cde94cb91a9214bdddf1a3517302add33e65a
Michel Falk <michel.falk@gmail.com>
2014-05-27 08:52:47 +0200

    Lagt till felmeddelanden

--- SputifyGUI/src/se/mah/patmic/sputifygui/PlayActivity.java
+++ SputifyGUI/src/se/mah/patmic/sputifygui/PlayActivity.java
@@ -1,14 +1,10 @@
 package se.mah.patmic.sputifygui;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import android.support.v7.app.ActionBarActivity;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,8 +19,7 @@ import android.view.View.OnClickListener;
  * 
  * @author Andreas
  * 
- *         This class plays songs by pressing a play button and pausing the song
- *         by pressing the pause button
+ *         This class plays songs by pressing a play button and pausing the song by pressing the pause button
  */
 public class PlayActivity extends ActionBarActivity {
 
@@ -38,7 +33,6 @@ public class PlayActivity extends ActionBarActivity {
 	private AlertDialog currentAlertDialog = null;
 	private byte[] audioArray;
 	private BluetoothService btService;
-	private boolean sendSamplesThreadStarted = false;
 	private Thread sendSamplesThread;
 
 	public void onCreate(Bundle savedInstanceState) {
@@ -50,17 +44,15 @@ public class PlayActivity extends ActionBarActivity {
 		new Thread(new InitAudioTrackThread()).start();
 
 		Intent startIntent = getIntent();
-		trackName = startIntent
-				.getStringExtra(PlaylistActivity.EXTRA_TRACK_NAME);
+		trackName = startIntent.getStringExtra(PlaylistActivity.EXTRA_TRACK_NAME);
 
 		try {
 			initViews();
 			setListeners();
 		} catch (Exception e) {
 			e.printStackTrace();
-			Toast.makeText(getApplicationContext(),
-					e.getClass().getName() + " " + e.getMessage(),
-					Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(), e.getClass().getName() + " " + e.getMessage(), Toast.LENGTH_LONG)
+					.show();
 		}
 	}
 
@@ -115,7 +107,7 @@ public class PlayActivity extends ActionBarActivity {
 			sendSamplesThread.start();
 
 		} else {
-			// TODO Track not loaded
+			showErrorMessage("Error", "File not loaded");
 		}
 	}
 
@@ -124,8 +116,22 @@ public class PlayActivity extends ActionBarActivity {
 			audioTrack.pause();
 			sendSamplesThread.interrupt();
 		} else {
-			// TODO Track not loaded
+			showErrorMessage("Error", "File not loaded");
 		}
+	}
+
+	private void showErrorMessage(final String title, final String message) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				if (currentAlertDialog != null && currentAlertDialog.isShowing()) {
+					currentAlertDialog.cancel();
+				}
+				currentAlertDialog = new AlertDialog.Builder(PlayActivity.this).setTitle(title).setMessage(message)
+						.setNeutralButton(android.R.string.ok, null).setIcon(android.R.drawable.ic_dialog_alert).show();
+			}
+		});
+
 	}
 
 	@Override
@@ -154,16 +160,13 @@ public class PlayActivity extends ActionBarActivity {
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						if (currentAlertDialog != null
-								&& currentAlertDialog.isShowing()) {
+						if (currentAlertDialog != null && currentAlertDialog.isShowing()) {
 							currentAlertDialog.cancel();
 						}
-						currentAlertDialog = new AlertDialog.Builder(
-								PlayActivity.this).setTitle("No track")
+						currentAlertDialog = new AlertDialog.Builder(PlayActivity.this).setTitle("No track")
 								.setMessage("Track not recieved from server")
 								.setNeutralButton(android.R.string.ok, null)
-								.setIcon(android.R.drawable.ic_dialog_alert)
-								.show();
+								.setIcon(android.R.drawable.ic_dialog_alert).show();
 					}
 				});
 				while (tcpConnection.getRequestedTrackStatus() == TCPConnection.TRACK_NOT_RECIEVED) {
@@ -179,16 +182,12 @@ public class PlayActivity extends ActionBarActivity {
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						if (currentAlertDialog != null
-								&& currentAlertDialog.isShowing()) {
+						if (currentAlertDialog != null && currentAlertDialog.isShowing()) {
 							currentAlertDialog.cancel();
 						}
-						currentAlertDialog = new AlertDialog.Builder(
-								PlayActivity.this).setTitle("Downloading")
-								.setMessage("Downloading track from server")
-								.setCancelable(false)
-								.setIcon(android.R.drawable.ic_dialog_alert)
-								.show();
+						currentAlertDialog = new AlertDialog.Builder(PlayActivity.this).setTitle("Downloading")
+								.setMessage("Downloading track from server").setCancelable(false)
+								.setIcon(android.R.drawable.ic_dialog_alert).show();
 					}
 				});
 				while (tcpConnection.getRequestedTrackStatus() == TCPConnection.TRACK_DOWNLOADING) {
@@ -203,8 +202,7 @@ public class PlayActivity extends ActionBarActivity {
 			runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-					if (currentAlertDialog != null
-							&& currentAlertDialog.isShowing()) {
+					if (currentAlertDialog != null && currentAlertDialog.isShowing()) {
 						currentAlertDialog.cancel();
 					}
 				}
@@ -223,8 +221,7 @@ public class PlayActivity extends ActionBarActivity {
 					int channels, encoding, samplerate, buffersize;
 
 					// Kollar att filen är i WAV format
-					if (new String(audioArray, 0, 4).equals("RIFF")
-							&& new String(audioArray, 8, 4).equals("WAVE")) {
+					if (new String(audioArray, 0, 4).equals("RIFF") && new String(audioArray, 8, 4).equals("WAVE")) {
 
 						// Kontrollerar att filen innehåller format beskrivning
 						if (new String(audioArray, 12, 4).equals("fmt ")) {
@@ -235,32 +232,27 @@ public class PlayActivity extends ActionBarActivity {
 								// kontrollerar bits/sample
 								if (audioArray[34] == 16 && audioArray[35] == 0) {
 									encoding = AudioFormat.ENCODING_PCM_16BIT;
-								} else if (audioArray[34] == 8
-										&& audioArray[35] == 0) {
+								} else if (audioArray[34] == 8 && audioArray[35] == 0) {
 									encoding = AudioFormat.ENCODING_PCM_8BIT;
 								} else {
 									encoding = AudioFormat.ENCODING_INVALID;
-									// TODO PRINTA UT ROSTAT BRÖD: Unsuported
-									// bit/sample
+									showErrorMessage("Unsuported format", "The bits-per-sample rate on this file is not supported");
 									return;
 								}
 							} else {
 								encoding = AudioFormat.ENCODING_INVALID;
-								// TODO PRINTA UT ROSTAT BRÖD: Unsuported
-								// encoding
+								showErrorMessage("Unsuported format", "The encoding on this file is not supported");
 								return;
 							}
 
 							// kontrollerar att audio är mono eller stereo
 							if (audioArray[22] == 2 && audioArray[23] == 0) {
 								channels = AudioFormat.CHANNEL_OUT_STEREO;
-							} else if (audioArray[22] == 1
-									&& audioArray[23] == 0) {
+							} else if (audioArray[22] == 1 && audioArray[23] == 0) {
 								channels = AudioFormat.CHANNEL_OUT_MONO;
 							} else {
 								channels = AudioFormat.CHANNEL_INVALID;
-								// TODO PRINTA UT ROSTAT BRÖD: Unsuported
-								// channels
+								showErrorMessage("Unsuported format", "The number of channels on this file is not supported");
 								return;
 							}
 
@@ -287,19 +279,16 @@ public class PlayActivity extends ActionBarActivity {
 							buffersize += temp * 0x1000000;
 
 							// Initierar audioTrack
-							audioTrack = new AudioTrack(
-									AudioManager.STREAM_MUSIC, samplerate,
-									channels, encoding, buffersize,
-									AudioTrack.MODE_STATIC);
+							audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, samplerate, channels, encoding,
+									buffersize, AudioTrack.MODE_STATIC);
 							audioTrack.write(audioArray, 44, buffersize);
 							audioTrackInitiated = true;
 						} else {
-							// TODO PRINTA UT ROSTAT BRÖD: Missing or corrupt
-							// header
+							showErrorMessage("Unsuported format", "Could not find file-header");
 							return;
 						}
 					} else {
-						// TODO PRINTA UT ROSTAT BRÖD: Unsuported file format
+						showErrorMessage("Unsuported format", "Unsupported file-type");
 						return;
 					}
 				} else {
@@ -310,9 +299,8 @@ public class PlayActivity extends ActionBarActivity {
 	}
 
 	/**
-	 * Denna tråd kommer att används för att räkna ut medelvärted av amplituder
-	 * under ett viss intervall och sedan anropa Bluetoothservice för att skicka
-	 * detta värde till arduino.
+	 * Denna tråd kommer att används för att räkna ut medelvärted av amplituder under ett viss intervall och sedan
+	 * anropa Bluetoothservice för att skicka detta värde till arduino.
 	 * 
 	 * @author Patrik
 	 * 
@@ -331,7 +319,7 @@ public class PlayActivity extends ActionBarActivity {
 		public int calculateAverage(byte[] array, int startIndex, int endIndex) {
 			int sum = 0;
 			int temp;
-			for (int i = startIndex; i < endIndex; i++) {
+			for (int i = startIndex; i < endIndex && i < array.length; i++) {
 				temp = array[i];
 				if (temp < 0) {
 					temp *= -1;
